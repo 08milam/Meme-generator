ee279b470dcd54b8a3fcf7ba136364a1f7141e9f
Matthew Milam <96691690+08milam@users.noreply.github.com>
2023-12-30 14:05:19 -0600

    change all code

--- script.js
+++ script.js
@@ -1,28 +1,99 @@
-//Made By: Charles M Milam Jr | Date: 10-14-23 | Memegenerator.js
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
- // top text input
-const topTextIN = document.querySelector("#topTextIN");
-const topTextOUT = document.querySelector('#topTextOUT');
-// buttom text input
-const bottomTextIN = document.querySelector("#bottomtextIN");
-const bottomTextOUT = document.querySelector('#bottomTextOUT');
-// image input
-const imageIN = document.querySelector("#imageIN");
 
-function creatememe(topTextIN, bottomTextIN, imageIN){
-    // all text output
 
-    topTextOUT.innerText = topTextIN;
-    bottomTextOUT.innerText = bottomTextIN;
-    // image output
-    let img = document.createElement('img');
-    img.src = URL.createObjectURL(imageIN.files[0]);
-    document.body.appendChild(img);
+// SUBMISSIONS CONTENT
+submissions = function(){
+    topText = document.querySelector('#topText').value
+        topParagraph.textContent = topText
+    bottomText = document.querySelector("#bottomText").value
+        bottomParagraph.textContent = bottomText
+    urlImage = document.querySelector('#urlImage').value
+        createImgTag.src = urlImage
 }
+// CREATE DIV TAG
+creatediv = function(){
+    main = document.querySelector('.main');
+        div = document.createElement('div');
+        div.setAttribute("class", "contentcontainer");
+        main.appendChild(div);
+};
+// IMAGE CONTAINER
+createImageContainer = function(){
+    divImg = document.createElement('div');
+    divImg.setAttribute("class", "imgContainer");
+    div.appendChild(divImg);
+};
+// TOP CONTAINER
+createTopContainer = function(){
+    topTextContent = document.createElement('div');
+    topTextContent.setAttribute("class", "topContainer");
+    div.appendChild(topTextContent);
+};
+// TOP PARAGRAPH
+creatTopParagraph= function(){
+    topParagraph = document.createElement('p');
+    topTextContent.appendChild(topParagraph);
+};
+// B0TTOM CONTAINER
+createBottomContainer = function(){
+    bottomTextContent = document.createElement('div');
+    bottomTextContent.setAttribute("class", "bottomContainer");
+    div.appendChild(bottomTextContent);
+};
+// BUTTON PARAGRAPH
+createButtonParagraph = function(){
+    bottomParagraph = document.createElement('p');
+    bottomTextContent.appendChild(bottomParagraph);
+};
+// CERATE IMG TAG
+createImg = function(){
+    createImgTag = document.createElement('img');
+    createImgTag.setAttribute('class', 'urlimage');
+    divImg.appendChild(createImgTag);
+};
+// DELETE ITEM
+drop = function(){
+    createbutton = document.createElement("button");
+    createbutton.setAttribute('class', "remove");
+    createbutton.textContent = "x";
+    div.appendChild(createbutton);
+    createbutton.addEventListener('click', function(e){
+        e.target.parentElement.remove(); 
+    });
+};
 
-imageIN.addEventListener('change', () => {
-    creatememe(topTextIN.value, bottomTextIN.value, imageIN);
-})
-
- 
+// IF FORM IS EMPTY
+formValidation = function(){
+// Check if the input value is empty or contains only whitespace
+if (urlImage.value === "" ) {
+    alert("Image input is empty. Please enter a value.")
+        // If the input is empty
+        if (bottomText.value === "") {
+            // If the input is empty
+            alert("bottom input is empty. Please enter a value.")
+            if (topText.value === "") {
+                // If the input is empty
+                alert("Top input is empty. Please enter a value.")
+               }
+           }
+    } else {
+// TEST FUNCTIONS BELOW 
+    creatediv();
+    createTopContainer();
+    creatTopParagraph();
+    createBottomContainer();
+    createButtonParagraph();
+    createImageContainer();
+    createImg();
+    drop();
+    submissions();
+        }
+}
+// FORM SUBMISSION EVENT
+const form = document.querySelector('#form');
+form.addEventListener('submit' || 'keydown', function(e) {
+  e.preventDefault();
+  formValidation();
+  function submitForm() {
+}
+});
